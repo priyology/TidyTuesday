025b6981f3e90a7c7b898b4e2a7aca48364f6b06
Priya Shukla <prishukla.sci@gmail.com>
2020-03-15 15:29:30 -0700

    Using advice I received on Twitter, I've ...
    1. pushed my most up-to-date script
    2. used bar plots instead of a pie chart
    3. used geofacet to visualize how demographic enrollment varies by state

--- scripts/10Mar2020_StudentTuition.R
+++ scripts/10Mar2020_StudentTuition.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(ggplot2)
+library(ggrepel)
 library(scales)
+library(geofacet)
 
-#### Question: What % of each diversity 'category' is enrolled in each state? ====
+#### Question: What % of each diversity 'category' is enrolled in each state ... without women / 2+ races? ====
 
 #### load data ====
 diversity_school <- read.csv("data/diversity_school.csv", header = TRUE)
@@ -11,24 +13,78 @@ glimpse(diversity_school)
 
 #### make variable 'category' a character variable & 'enrollment' an integer ====
 diversity_school$category <- as.character(diversity_school$category)
-diversity_school$enrollment <- as.integer(diversity_school$enrollment)
+diversity_school$state <- as.character(diversity_school$state)
+diversity_school$enrollment <- as.double(diversity_school$enrollment)
 
-#### create a column for % enrollment per institution ====
-enrollment_percent <- diversity_school %>%
-  mutate(enrollment / total_enrollment) %>% 
-  pull()
+#### Create a data.frame without total_enrollment column because it's unclear how that number was calculated ====
+diversity_school <- diversity_school %>%
+  select(name, state, category, enrollment)
+View(diversity_school)
+glimpse(diversity_school)
 
-#### create a new data.frame with diversity & enrollment_percent for all categories within each state/university ====
-diversity_school <- cbind(diversity_school, enrollment_percent)
-
-#### break up enrollment by state ====
-total_enrollment.state <- diversity_school %>% 
-  group_by(state) %>% 
-  summarise((sum(unique(total_enrollment))))%>%
+#### omit "women", "2+ races", "total minority" as a category in variable ====
+category.enrollment <-  filter(diversity_school, category != "Women" & category != "Two Or More Races" & category != "Total Minority") %>%
+  arrange(state) %>%
   ungroup %>%
   as.data.frame()
 
-View(total_enrollment.state)
+View(category.enrollment)
+
+#### break up enrollment by state, this gives us how many individuals are enrolled that belong to each category ====
+enrollment.state <- category.enrollment %>% 
+ filter(state != "NA") %>% 
+  group_by(state, category) %>%
+  summarize(sum(enrollment)) %>% 
+  ungroup %>%
+  as.data.frame()
+
+colnames(enrollment.state)[colnames(enrollment.state)=="sum(enrollment)"] <- "enrollment"
+View(enrollment.state)
+
+## create column w/ state abbreviations"
+
+state.abbr <- state.abb[match(enrollment.state$state, state.name)]
+enrollment.state <- cbind(state.abbr, enrollment.state)
+
+View(enrollment.state)
+
+#### Plot chart that shows enrollment of each category by state ====
+
+Enrollment.Plot <- ggplot(enrollment.state, aes(category, enrollment, fill = category)) +
+  geom_col() +
+  geom_text(aes(category, enrollment, label = enrollment), hjust = -0.5, size = 3,
+            position = position_dodge(width = 1),
+            inherit.aes = TRUE) +
+  coord_flip() +
+  scale_y_continuous(name ="Enrollment Numbers", 
+                     breaks=c(0, 100000)) +
+  theme_classic() +
+  theme(axis.text.x = element_blank()) +
+  facet_geo(~ state.abbr) +
+  labs(title = "How Enrollment Demographics Vary By State",
+       subtitle = "Plotted using geom_col & geofacet",
+       caption = "#TidyTuesday Take 2 Based on Advice received on Twitter")
+
+Enrollment.Plot
+
+png('fig_output/EnrollmentPlot_15Mar2020.png',width=1000,height=1000,units="px")
+
+print(Enrollment.Plot)
+
+dev.off()
+
+
+##########################################
+
+
+#### other random code from playing around: breakdown of data by women ==== 
+
+#### merge the total_enrollment.state & enrollment.category date.frames & rename columns ====
+state_category.enrollment<-merge(x=enrollment.state,y=category.enrollment, by="category", "state")
+
+View(state_category.enrollment)
+
+
 
 
 #### break up enrollment by category ====
@@ -85,25 +141,23 @@ category.enrollment.eth <- cbind(category.enrollment.eth, enrollment.percent.eth
 
 View(category.enrollment.eth)
 
-#### Plot chart that shows enrollment of each category by state ====
 
-CategoryEnrollment.Plot <- ggplot(category.enrollment.eth, aes(x="", y=enrollment.percent.eth, fill=category)) +
-  geom_bar(stat="identity", width=1, color="white") +
-  facet_wrap(. ~ state) +
-  coord_polar("y", start=0) +
-  labs(title="Demographics of College Student Enrollment By State",fill = "Student Demographics") +
-  theme_void() # remove background, grid, numeric labels
+#### create a column for % enrollment per institution ====
+enrollment_percent <- diversity_school %>%
+  mutate(enrollment / total_enrollment) %>% 
+  pull()
 
-CategoryEnrollment.Plot
+#### create a new data.frame with diversity & enrollment_percent for all categories within each state/university ====
+diversity_school <- cbind(diversity_school, enrollment_percent)
 
-png('fig_output/CollegeStudentEnrollmentByState_11Mar2020.png',width=1000,height=1000,units="px",bg = "transparent")
+#### break up enrollment by state ====
+total_enrollment.state <- diversity_school %>% 
+  group_by(state) %>% 
+  summarise((sum(unique(total_enrollment))))%>%
+  ungroup %>%
+  as.data.frame()
 
-print(CategoryEnrollment.Plot)
-
-dev.off()
-
-
-#### other random code from playing around: breakdown of data by women ==== 
+View(total_enrollment.state)
 
 #### % women enrolled in institutions
 women.enrollment <-  filter(diversity_school, category == "Women") %>%

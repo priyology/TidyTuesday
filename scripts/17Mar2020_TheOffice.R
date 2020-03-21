0534732c3d2a5e5db3c4ebf028a55ab093cf4823
Priya Shukla <prishukla.sci@gmail.com>
2020-03-21 16:04:29 -0700

    Committing one final time :)

--- scripts/17Mar2020_TheOffice.R
+++ scripts/17Mar2020_TheOffice.R
@@ -6,7 +6,7 @@
 #install.packages("schrute")
 #install.packages("tidytext")
 #install.packages("tokenizers")
-#install.packages("ggforce")
+#install.packages("ggpubr")
 
 #### Load Packages ====
 
@@ -14,7 +14,8 @@ library(schrute)
 library(tidytext)
 library(tidyverse)
 library(tokenizers)
-library(ggforce)
+library(ggpubr)
+library(fishualize)
 
 #### Create data.frame & get a sense of the data ====
 OfficeData <- theoffice
@@ -29,16 +30,67 @@ OfficeData <- cbind(OfficeData, sentences)
 
 FilterSheSaid <- OfficeData[grep("that's what she said", OfficeData$sentences), ]
 
+FilterSheSaid$season <- as.integer(FilterSheSaid$season)
+
+View(FilterSheSaid)
+glimpse(FilterSheSaid)
+
+
+#### Plot the frequency of "that's what she said" per season ====
+
+png("fig_output/SheSaidPlot_21Mar2020.png")
+
+SheSaid.Plot <- ggplot(FilterSheSaid, aes(x = season, fill = factor(season))) +
+  geom_bar(width = 0.5) +
+  scale_x_continuous(name = "Season", breaks = seq(0, 9, 1)) +
+  scale_y_continuous(name="'That's what she said.' frequency", breaks = seq(0, 10, 1)) +
+  scale_fill_fish_d(option = "Lepomis_megalotis") +
+  guides(fill=FALSE) +
+  ggtitle("# of Times A Character on 'The Office' said 'That's what she said.' ") +
+  theme_classic() +
+  theme(plot.title = element_text(hjust = 0.5, size = 16))
+
+print(SheSaid.Plot)
+dev.off()
+
+
+#### Plot the frequency of Michael saying "that's what she said" per season ====
+
+SheSaid.Michael <- FilterSheSaid %>% 
+filter(character == 'Michael') %>%
+ungroup %>%
+as.data.frame()
+
+View(SheSaid.Michael)
+
+png("fig_output/MichaelPlot21Mar2020.png")
+
+Michael.Plot <- ggplot(SheSaid.Michael, aes(x = season, fill = factor(season))) +
+  geom_bar(width = 0.5) +
+  scale_x_continuous(name = "Season", breaks = seq(0, 9, 1)) +
+  scale_y_continuous(name="'That's what she said.' frequency", breaks = seq(0, 10, 1)) +
+  scale_fill_fish_d(option = "Hypsypops_rubicundus") +
+  guides(fill=FALSE) +
+  ggtitle("# of Times Michael from 'The Office' said 'That's what she said.' ") +
+  theme_classic() +
+  theme(plot.title = element_text(hjust = 0.5, size = 16))
+
+print(Michael.Plot)
+dev.off()
+
+#### Other Random code ==== 
+
+
 #### How many times is "that's what she said uttered during each season ====
-SheSaidCount <- FilterSheSaid %>% 
-  group_by(season) %>%
-  summarize(length(sentences)) %>%
-  ungroup %>%
-  as.data.frame()
+#SheSaidCount <- FilterSheSaid %>% 
+# group_by(season) %>%
+#summarize(length(sentences)) %>%
+#ungroup %>%
+#as.data.frame()
 
-colnames(SheSaidCount)[colnames(SheSaidCount)=="length(sentences)"] <- "num.utterances"
+#colnames(SheSaidCount)[colnames(SheSaidCount)=="length(sentences)"] <- "num.utterances"
 
-View(SheSaidCount)
+#View(SheSaidCount)
 
 #### merge the # of utterances with the original dataset ====
 #FilterSheSaid <- merge(FilterSheSaid, SheSaidCount)
@@ -46,18 +98,5 @@ View(SheSaidCount)
 #View(FilterSheSaid)
 
 #make sure the numerical characters are just numeric
-FilterSheSaid$num.utterances <- as.integer(FilterSheSaid$num.utterances)
-FilterSheSaid$season <- as.integer(FilterSheSaid$season)
+#FilterSheSaid$num.utterances <- as.integer(FilterSheSaid$num.utterances)
 
-glimpse(FilterSheSaid)
-
-
-#### Plot the frequency of "that's what she said" per season ====
-
-SheSaid.Plot <- ggplot(SheSaidCount, aes(num.utterances)) +
-  geom_bar() +
-  scale_x_discrete(name ="Season of 'The Office'", limits=c("1", "2", "3", "4", "5", "6", "7", "8", "9")) +
-  scale_y_discrete(name="# of times a character said 'That's what she said.'", limits=c("0", "1", "2", "3", "4", "5", "6", "7")) +
-  theme_classic()
-
-print(SheSaid.Plot)
